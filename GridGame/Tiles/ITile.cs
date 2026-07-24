0122c2d516dadc5ac279b6b1869c4216be4eead5
Shivam <shivam.engineer1@gmail.com>
2026-07-24 18:44:16 -0400

    Started making tile types

---
+++ GridGame/Tiles/ITile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.Tiles {
+    public interface ITile {
+        public void GetResources();
+
+        public void SetTile(ITile tile);
+    }
+}
