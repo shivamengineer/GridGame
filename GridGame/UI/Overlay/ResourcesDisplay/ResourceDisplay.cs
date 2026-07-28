245313f22a27ce6eb8e41e678529d48dad723a0c
Shivam <shivam.engineer1@gmail.com>
2026-07-28 02:55:32 -0400

    Started creating resource counts display

---
+++ GridGame/UI/Overlay/ResourcesDisplay/ResourceDisplay.cs
@@ -0,0 +1,24 @@
+﻿using GridGame.UI.Elements;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.UI.Overlay.ResourcesDisplay {
+    public class ResourceDisplay {
+
+        private List<IItem> resourceItems;
+
+        public ResourceDisplay() {
+            resourceItems = new List<IItem>();
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            //foreach IItem item in resourceItems
+            //item.Draw
+        }
+
+    }
+}
