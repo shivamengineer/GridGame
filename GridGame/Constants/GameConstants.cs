529bb771eb0013d7a0c5712ae1aad0ab68cbbe19
Shivam <shivam.engineer1@gmail.com>
2026-07-28 02:48:53 -0400

    Adjusted screen size to be closer to full screen

--- GridGame/Constants/GameConstants.cs
+++ GridGame/Constants/GameConstants.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace GridGame.Constants {
     public static class GameConstants {
-        public static int WINDOW_WIDTH = 1600;
-        public static int WINDOW_HEIGHT = 800;
-
+        public static int WINDOW_WIDTH = 1920;
+        public static int WINDOW_HEIGHT = 960;
     }
 }
