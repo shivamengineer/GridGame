44ea57fcca073c907aed8a6f812c693c3933e132
Shivam <shivam.engineer1@gmail.com>
2026-07-28 17:09:19 -0400

    Added constants class for UI info

---
+++ GridGame/Constants/UIOverlayDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.Constants {
+    public static class UIOverlayDetails {
+
+        public static int RESOURCE_BAR_HEIGHT = 64;
+        public static int RESOURCE_BAR_PADDING = 20;
+
+    }
+}
