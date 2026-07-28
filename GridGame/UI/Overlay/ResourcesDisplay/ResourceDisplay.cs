e940db8a5b866138a4e16e5afec8000717df75ee
Shivam <shivam.engineer1@gmail.com>
2026-07-28 17:24:45 -0400

    Started generating resource display

--- GridGame/UI/Overlay/ResourcesDisplay/ResourceDisplay.cs
+++ GridGame/UI/Overlay/ResourcesDisplay/ResourceDisplay.cs
@@ -1,4 +1,6 @@
-﻿using GridGame.UI.Elements;
+﻿using GridGame.Resources;
+using GridGame.UI.Elements;
+using GridGame.UI.Elements.Items;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,20 @@ using System.Threading.Tasks;
 namespace GridGame.UI.Overlay.ResourcesDisplay {
     public class ResourceDisplay {
 
-        private List<IItem> resourceItems;
+        private Dictionary<ResourceType, IItem> resourceItems;
 
         public ResourceDisplay() {
-            resourceItems = new List<IItem>();
+            InitializeResources();
+        }
+
+        private void InitializeResources() {
+            resourceItems = new Dictionary<ResourceType, IItem> {
+                [ResourceType.Food] = new FoodItem(),
+                [ResourceType.Gold] = new GoldItem(),
+                [ResourceType.Morale] = new MoraleItem(),
+                [ResourceType.Production] = new ProductionItem(),
+                [ResourceType.Science] = new ScienceItem(),
+            };
         }
 
         public void Draw(SpriteBatch spriteBatch) {
