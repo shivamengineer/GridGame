8d5fd48c011dc844eb2889e2b715feb0f7b53a6b
Shivam <shivam.engineer1@gmail.com>
2026-07-30 16:29:36 -0400

    Updated starting resources to ensure backend and visual are connected

--- GridGame/Constants/StartingResources.cs
+++ GridGame/Constants/StartingResources.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace GridGame.Constants {
     public static class StartingResources {
 
-        public static int STARTING_FOOD = 0;
-        public static int STARTING_GOLD = 0;
+        public static int STARTING_FOOD = 2;
+        public static int STARTING_GOLD = 2;
         public static int STARTING_MORALE = 5;
-        public static int STARTING_PRODUCTION = 0;
-        public static int STARTING_SCIENCE = 0;
+        public static int STARTING_PRODUCTION = 4;
+        public static int STARTING_SCIENCE = 2;
 
     }
 }
