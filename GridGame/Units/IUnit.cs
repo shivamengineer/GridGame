f59306951a92a6eee349ea9b3e49072a8bbfed28
Shivam <shivam.engineer1@gmail.com>
2026-08-06 17:30:00 -0400

    Created unit interface

---
+++ GridGame/Units/IUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.Units {
+    public interface IUnit {
+
+        public void MoveTo(int q, int r);
+
+        public void MoveUp();
+
+        public void MoveDown();
+
+        public void MoveUpRight();
+
+        public void MoveDownRight();
+
+        public void MoveUpLeft();
+
+        public void MoveDownLeft();
+
+    }
+}
