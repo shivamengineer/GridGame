d46f669834020ca3ed4e66e126fd36d1e0acf0c3
Shivam <shivam.engineer1@gmail.com>
2026-08-18 15:13:35 -0400

    Added citizen productivity stat

--- GridGame/Constants/Resources/CityBaseStats.cs
+++ GridGame/Constants/Resources/CityBaseStats.cs
@@ -10,8 +10,10 @@ namespace GridGame.Constants.Resources {
         public static int FOOD_RATE = 2;
         public static int GOLD_RATE = 2;
         public static int MORALE_RATE = 0;
-        public static int PRODUCTION_RATE = 2;
+        public static int PRODUCTION_RATE = 0;
         public static int SCIENCE_RATE = 1;
+
+        public static int CITIZEN_BASE_PRODUCTIVITY = 1;
 
     }
 }
