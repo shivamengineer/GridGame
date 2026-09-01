6d4be8f9cbb0b5db9981c7d112a973aecfacb328
Shivam <shivam.engineer1@gmail.com>
2026-09-01 17:21:02 -0400

    Fixed class name

--- GridGame/Constants/Treatment/HealthEffectStats.cs
+++ GridGame/Constants/Treatment/HealthEffectStats.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GridGame.Constants.Treatment {
-    public static class HealthEffects {
+    public static class HealthEffectStats {
 
         public static float DROWSY_STRENGTH = 0.1f;
         public static float HYDRATED_WATER_STRENGTH = 0.35f;
