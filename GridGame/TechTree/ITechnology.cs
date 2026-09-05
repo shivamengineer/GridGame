4dc52160446751201f5315fe13c5d82a26a42821
Shivam <shivam.engineer1@gmail.com>
2026-09-05 17:37:02 -0400

    Adding tech tree

---
+++ GridGame/TechTree/ITechnology.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.TechTree {
+    public interface ITechnology {
+        
+        public HashSet<ITechnology> Prerequisites { get; set; }
+        public HashSet<ITechnology> NextTechs { get; set; }
+
+    }
+}
---
+++ GridGame/TechTree/TechController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.TechTree {
+    public class TechController {
+
+        private HashSet<ITechnology> ResearchedTech;
+        private HashSet<ITechnology> ResearchableTech;
+
+        public TechController() {
+            ResearchedTech = new HashSet<ITechnology>();
+            ResearchableTech = new HashSet<ITechnology>();
+        }
+
+        public bool HasTechs(HashSet<ITechnology> techs) {
+            foreach(ITechnology tech in techs) {
+                if(!ResearchedTech.Contains(tech)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
