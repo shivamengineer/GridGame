a5cff649f6cd4c26aaae04e584fccff5a3b57973
Shivam <shivam.engineer1@gmail.com>
2026-09-04 21:39:30 -0400

    changed covid stats to be readonly

--- GridGame/Constants/Viruses/Covid/CovidStats.cs
+++ GridGame/Constants/Viruses/Covid/CovidStats.cs
@@ -8,22 +8,22 @@ using System.Threading.Tasks;
 namespace GridGame.Constants.Viruses.Covid {
     public static class CovidStats {
 
-        public static float MIN_STRENGTH = 0.2f;
-        public static float STRENGTH_RANGE = 0.5f;
+        public static readonly float MIN_STRENGTH = 0.2f;
+        public static readonly float STRENGTH_RANGE = 0.5f;
 
-        public static float VIRUS_BASE_DURATION = 18f; //26f
+        public static readonly float VIRUS_BASE_DURATION = 18f; //26f
 
-        public static float VIRUS_CHANCE_TO_SURVIVE = 0.9f;
-        public static float VIRUS_SURVIVE_MULTIPLIER = 0.85f;
+        public static readonly float VIRUS_CHANCE_TO_SURVIVE = 0.9f;
+        public static readonly float VIRUS_SURVIVE_MULTIPLIER = 0.85f;
 
-        public static float SPREAD_CHANCE = 0.75f;
-        public static int SPREAD_RANGE = 2;
+        public static readonly float SPREAD_CHANCE = 0.75f;
+        public static readonly int SPREAD_RANGE = 2;
 
-        public static float TIME_TO_SPREAD = 8f;
+        public static readonly float TIME_TO_SPREAD = 8f;
 
-        public static float TIME_BEFORE_OUTBREAK = 12f;
-        public static float ASYMPTOMATIC_TIME = 10f;
+        public static readonly float TIME_BEFORE_OUTBREAK = 16f;
+        public static readonly float ASYMPTOMATIC_TIME = 10f;
 
-        public static float MORTALITY_RATE = 0.1f;
+        public static readonly float MORTALITY_RATE = 0.1f;
     }
 }
