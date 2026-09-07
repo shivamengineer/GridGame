d2cb46fed51eca5a6b605e83012a5588371715f2
Shivam <shivam.engineer1@gmail.com>
2026-09-06 22:04:15 -0400

    Added class for getting a new ITechnology from the enum type

---
+++ GridGame/TechTree/Backend/NewTechnology.cs
@@ -0,0 +1,24 @@
+﻿using GridGame.TechTree.Backend.TechnologyClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.TechTree.Backend {
+    public class NewTechnology {
+
+        private Dictionary<TechnologyTypes, ITechnology> TechnologyMap;
+
+        public NewTechnology() {
+            TechnologyMap = new Dictionary<TechnologyTypes, ITechnology>() {
+                [TechnologyTypes.FACEMASK] = new FacemaskTechnology(),
+            };
+        }
+
+        public ITechnology GetTechnology(TechnologyTypes type) {
+            return TechnologyMap[type].NewInstance();
+        }
+
+    }
+}
