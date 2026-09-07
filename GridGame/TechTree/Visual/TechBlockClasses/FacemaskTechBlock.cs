c3f29cac6d0919358a87375657d9ad93cb63270b
Shivam <shivam.engineer1@gmail.com>
2026-09-07 17:35:17 -0400

    Created first tech block class

---
+++ GridGame/TechTree/Visual/TechBlockClasses/FacemaskTechBlock.cs
@@ -0,0 +1,40 @@
+﻿using GridGame.TechTree.Backend;
+using GridGame.TechTree.Backend.Technology.TechnologyClasses;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGame.TechTree.Visual.TechBlockClasses {
+    public class FacemaskTechBlock : AbstractTechBlock {
+
+        public FacemaskTechBlock() {
+            TechType = TechnologyTypes.FACEMASK;
+            Technology = new FacemaskTechnology();
+            InitializeConnectedTechBlocks();
+
+        }
+
+        private void InitializeConnectedTechBlocks() {
+            Prerequisites = new HashSet<TechnologyTypes>() {
+                //
+            };
+            NextTechs = new HashSet<TechnologyTypes>() {
+                //
+            };
+        }
+
+        
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            //
+        }
+
+        public override ITechBlock NewInstance() {
+            return new FacemaskTechBlock();
+        }
+
+    }
+}
