78a5a5862041a1b8f645601a901493674b5e823b
Shivam <shivam.engineer1@gmail.com>
2026-09-12 01:48:40 -0400

    Now only draws name of the tech if it is viisble

--- GridGame/TechTree/Visual/TechnologyBlocks/AbstractTechBlock.cs
+++ GridGame/TechTree/Visual/TechnologyBlocks/AbstractTechBlock.cs
@@ -116,6 +116,7 @@ namespace GridGame.TechTree.Visual.TechnologyBlocks {
             }
 
             spriteBatch.Draw(backgroundTexture, Background, Color.Gray);
+            if(!TechStatus.Visible) return;
             string drawText = text;
             spriteBatch.DrawString(font, drawText, new Vector2(Background.X + 10, Background.Y + 10), Color.Red);
         }
